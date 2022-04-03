b4ecd7d83d9f321d69a391f7657f3ab2dcee356c
Leiv Erik Rosseland <leiv.rosseland@gmail.com>
2022-04-03 13:19:49 +0200

    better toddler roaming and cleanup file structure

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,26 +1,29 @@
 extends Node2D
 
 
-onready var toddler_area = $Toddler/Area2D
-onready var toddler = $Toddler
 onready var map = $Map
+onready var toddlers = $Toddlers
+onready var roaming_toddler_timer = $RoamingToddlerTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	start_roaming_toddler_timer()
 
 
-func _on_Toddler_request_path_to_other_room():
-	var current_room = map.room_from_toddler(toddler_area)
-	print(current_room)
+func _on_RoamingToddlerTimer_timeout():
+	route_toddler_to_room(toddlers.get_children()[randi() % toddlers.get_children().size()])
+	start_roaming_toddler_timer()
+
+
+func route_toddler_to_room(toddler):
+	var current_room = map.room_from_toddler(toddler.get_node("Area2D"))
 	var rooms = map.list_of_rooms()
 	var room = rooms[randi() % rooms.size()]
 	while room.name == current_room.name:
 		room = rooms[randi() % rooms.size()]
 	
 	toddler.receive_path_to_room(map.find_path_between_rooms(current_room, room, [], []))
-	
 
 
-func _on_Toddler_request_path_to_hazard():
-	pass # Replace with function body.
+func start_roaming_toddler_timer():
+	roaming_toddler_timer.start(rand_range(3, 8))
