a649491355bfe35f7bdf37cc91394c15af2cd0b1
DanielElisenberg <33904479+DanielElisenberg@users.noreply.github.com>
2022-04-04 22:32:27 +0200

    gameovermodal message updates

--- scripts/GameOverModal.gd
+++ scripts/GameOverModal.gd
@@ -3,15 +3,19 @@ extends Node2D
 
 onready var congratulations_message = $CongratulationsMessage
 onready var whoopsie_message = $WhoopsieMessage
+onready var victory_message = $VictoryMessage
 onready var numbers = $Numbers
 onready var modal = $Modal
 
 
 func display_congratulations_message(dead_toddlers: String):
 	modal.visible = true
-	congratulations_message.visible = true
-	numbers.visible = true
-	numbers.play(dead_toddlers)
+	if dead_toddlers == "0":
+		victory_message.visible = true
+	else:
+		congratulations_message.visible = true
+		numbers.visible = true
+		numbers.play(dead_toddlers)
 
 
 func display_whoopsie_message():
